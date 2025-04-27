c7e81c4ede719c45fa487262a34c84b96de697dc
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2025-04-27 15:15:44 +0300

    Added Project Files

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  providers: [provideClientHydration(withEventReplay()), provideHttpClient()],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
