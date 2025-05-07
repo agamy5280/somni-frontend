3fb18244be27b98acfbabdd88b22bb23f6524454
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2025-05-07 13:47:23 +0300

    FIX: Pinpoint not showing in geolocation

--- src/app/components/geolocation/geolocation.component.ts
+++ src/app/components/geolocation/geolocation.component.ts
@@ -16,6 +16,22 @@ import * as L from 'leaflet';
 import Fuse from 'fuse.js';
 import { Chart, registerables } from 'chart.js';
 
+import { icon, Marker } from 'leaflet';
+const iconRetinaUrl = 'assets/leaflet/marker-icon-2x.png';
+const iconUrl = 'assets/leaflet/marker-icon.png';
+const shadowUrl = 'assets/leaflet/marker-shadow.png';
+const iconDefault = icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41],
+});
+Marker.prototype.options.icon = iconDefault;
+
 // Register Chart.js components
 Chart.register(...registerables);
 
