5c1d40ebdd911b8aae77b38605092a5a7c2ad104
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2025-05-14 17:17:16 +0300

    edited files

--- start-both.sh
+++ start-both.sh
@@ -3,7 +3,7 @@
 npx json-server --watch src/assets/database/db.json --host 0.0.0.0 --port 3000 &
 JSON_SERVER_PID=$!
 
-# Start Angular app with proxy in the foreground
+# Start Angular app in the foreground
 node_modules/.bin/ng serve --host 0.0.0.0 --port 4200 --disable-host-check
 
 # Clean up JSON server when Angular app exits
