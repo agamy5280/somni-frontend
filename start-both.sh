6b06129e5f1e954b7b8627543e3ae6342247da64
Marwan ElAgamy <marawan.agamy2010@hotmail.com>
2025-05-14 16:26:08 +0300

    Add start script for running both services

---
+++ start-both.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Start JSON server in the background
+npx json-server --watch src/assets/database/db.json --host 0.0.0.0 --port 3000 &
+JSON_SERVER_PID=$!
+
+# Start Angular app in the foreground
+node_modules/.bin/ng serve --host 0.0.0.0 --port 4200 --disable-host-check
+
+# Clean up JSON server when Angular app exits
+kill $JSON_SERVER_PID
